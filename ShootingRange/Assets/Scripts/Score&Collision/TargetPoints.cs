1328f06b7321fd2ac661444f87e81fc20d2bc58e
Martin Kotschmar <martin.kotschmar@gmail.com>
2021-11-12 18:32:21 +0100

    add target respawn

--- ShootingRange/Assets/Scripts/Score&Collision/TargetPoints.cs
+++ ShootingRange/Assets/Scripts/Score&Collision/TargetPoints.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
+using System.Threading;
 
 public class TargetPoints : MonoBehaviour
 {
+
+    public GameObject[] targets;
+
+    public GameObject target;
+
+    private Vector3 oldPosition;
+
+    void Awake () 
+    {
+        targets = GameObject.FindGameObjectsWithTag("Target");
+    }
     private void OnCollisionEnter(Collision collision)
     {
+
+        int spawn = Random.Range(0, targets.Length);
+
         Debug.Log("was shot");
         if (collision.gameObject.tag == "Bullet")
         {
             KeepScore.Score += 100;
             Debug.Log("Hit the ball");
+            this.gameObject.transform.position = oldPosition;
             Destroy(this.gameObject);
+            Thread.Sleep(5);
+            GameObject.Instantiate(target, oldPosition, Quaternion.identity);
         }
+    
     }
 }
